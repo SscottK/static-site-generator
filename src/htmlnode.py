77f49325426dc3eecd856b57fe6ea6658cee7dc2
SscottK <gr8whtninjassk@gmail.com>
2025-03-10 21:30:16 -0700

    Add: method text>html, delimiter split----working

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,4 +1,4 @@
-
+from textnode import TextNode, TextType
 
 class HTMLNode():
     
@@ -9,13 +9,122 @@ class HTMLNode():
         self.props = props
 
     def to_html(self):
-        raise NotImplementedError
+        raise NotImplementedError("to_html method not implemented")
     
     def props_to_html(self):
-        if self.props is not None:
-            html_attributes = " " + " ".join(f'{key}="{value}"' for key, value in self.props.items())
-            return html_attributes
-        else:
-            return None
+        if self.props is None:            
+            return ""
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
     def __repr__(self):
         return f'HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})'
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+        self.props = props
+    
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("invalid HTML: no value")
+        if self.tag == None:
+            return self.value
+        
+        
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+    
+
+class ParentNode(HTMLNode):
+
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, props)
+        
+        self.children = children
+        self.props = props
+        
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("invalid HTML: no children")
+        childs = ""
+        for child in self.children:
+            
+            childs += child.to_html()
+        result = f"<{self.tag}{self.props_to_html()}>{childs}</{self.tag}>"
+        
+        return result
+    
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise ValueError(f"Invalid TextType: {text_node.text_type}")
+        
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    
+    
+
+    new_nodes = []
+    
+    for old_node in old_nodes:
+        # If not a TEXT node, keep as is
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+            
+        # Process TEXT nodes to find delimiters
+        text = old_node.text
+        result = []
+        
+        # Continue searching until no more delimiters
+        while delimiter in text:
+            # Find opening delimiter
+            start_index = text.find(delimiter)
+            if start_index == -1:
+                break
+                
+            # Find closing delimiter
+            end_index = text.find(delimiter, start_index + len(delimiter))
+            if end_index == -1:
+                raise ValueError(f"Closing delimiter not found: {delimiter}")
+                
+            # Extract parts
+            before_text = text[:start_index]
+            delimited_text = text[start_index + len(delimiter):end_index]
+            
+            # Add before_text if not empty
+            if before_text:
+                result.append(TextNode(before_text, TextType.TEXT))
+                
+            # Add delimited text with specified type
+            result.append(TextNode(delimited_text, text_type))
+            
+            # Update text to remaining portion
+            text = text[end_index + len(delimiter):]
+         
+        # Add any remaining text
+        
+        result.append(TextNode(text, TextType.TEXT))
+            
+        # Add all newly created nodes to the result
+        new_nodes.extend(result)
+            
+    return new_nodes
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,7 +1,7 @@
 import unittest
 
-from htmlnode import HTMLNode
-
+from htmlnode import HTMLNode, LeafNode, ParentNode, text_node_to_html_node, split_nodes_delimiter
+from textnode import TextNode, TextType
 
 class TestHTMLNode(unittest.TestCase):
     
@@ -51,5 +51,208 @@ class TestHTMLNode(unittest.TestCase):
             "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
         )
 
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://www.google.com">Click me!</a>',
+        )
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+    
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+    def test_parent_with_empty_children_list(self):
+        parent_node = ParentNode("div", [])
+        self.assertEqual(parent_node.to_html(), "<div></div>")
+
+    def test_multiple_children(self):
+        child1 = LeafNode("span", "first")
+        child2 = LeafNode("span", "second")
+        child3 = LeafNode("span", "third")
+        parent_node = ParentNode("div", [child1, child2, child3])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span>first</span><span>second</span><span>third</span></div>"
+        )
+
+    def test_deep_nesting(self):
+        leaf1 = LeafNode("b", "bold")
+        leaf2 = LeafNode("i", "italic")
+        mid_parent1 = ParentNode("p", [leaf1, leaf2])
+        leaf3 = LeafNode("a", "link")
+        mid_parent2 = ParentNode("div", [leaf3])
+        root = ParentNode("section", [mid_parent1, mid_parent2])
+        self.assertEqual(
+            root.to_html(),
+            "<section><p><b>bold</b><i>italic</i></p><div><a>link</a></div></section>"
+        )
+
+    def test_with_properties(self):
+        child = LeafNode("a", "click me", {"href": "https://boot.dev"})
+        parent = ParentNode("div", [child], {"class": "container"})
+        self.assertEqual(
+            parent.to_html(),
+            '<div class="container"><a href="https://boot.dev">click me</a></div>'
+        )
+    def test_missing_tag_raises_error(self):
+        with self.assertRaises(ValueError) as context:
+            ParentNode(None, [LeafNode("span", "child")]).to_html()
+        self.assertTrue("invalid HTML: no tag" in str(context.exception))
+
+    def test_missing_children_raises_error(self):
+        
+        parent = ParentNode("div", [LeafNode("span", "child")])
+        parent.children = None
+        with self.assertRaises(ValueError) as context:
+            parent.to_html()
+        self.assertTrue("invalid HTML: no children" in str(context.exception))
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_bold(self):
+        node = TextNode("This is bold text", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold text")
+
+    def test_italic(self):
+        node = TextNode("This is italic text", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is italic text")
+
+    def test_code(self):
+        node = TextNode("This is code", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is code")
+
+    def test_link(self):
+        node = TextNode("Click me", TextType.LINK, "https://www.example.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "Click me")
+        self.assertEqual(html_node.props["href"], "https://www.example.com")
+
+    def test_image(self):
+        node = TextNode("Alt text", TextType.IMAGE, "image.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")  # Empty string for image value
+        self.assertEqual(html_node.props["src"], "image.png")
+        self.assertEqual(html_node.props["alt"], "Alt text")
+    
+    def test_split(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        
+        assert split_nodes_delimiter([node], "`", TextType.CODE) == [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" word", TextType.TEXT),
+    
+            ]
+
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_simple_split(self):
+        # Test a simple case with one delimiter pair
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[0].text, "This is text with a ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "code block")
+        self.assertEqual(new_nodes[1].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[2].text, " word")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+
+    def test_multiple_splits(self):
+        # Test with multiple delimiter pairs
+        node = TextNode("Text with `code` and another `code block`", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(len(new_nodes), 5)
+        self.assertEqual(new_nodes[0].text, "Text with ")
+        self.assertEqual(new_nodes[1].text, "code")
+        self.assertEqual(new_nodes[1].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[2].text, " and another ")
+        self.assertEqual(new_nodes[3].text, "code block")
+        self.assertEqual(new_nodes[3].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[4].text, "")
+
+    def test_no_delimiter(self):
+        # Test when there's no delimiter in the text
+        node = TextNode("Plain text without delimiters", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(len(new_nodes), 1)
+        self.assertEqual(new_nodes[0].text, "Plain text without delimiters")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+
+    def test_non_text_node(self):
+        # Test when the node is not a TEXT type
+        node = TextNode("Already bold", TextType.BOLD)
+        new_nodes = split_nodes_delimiter([node], "*", TextType.ITALIC)
+        self.assertEqual(len(new_nodes), 1)
+        self.assertEqual(new_nodes[0].text, "Already bold")
+        self.assertEqual(new_nodes[0].text_type, TextType.BOLD)
+
+    def test_missing_closing_delimiter(self):
+        # Test when there's no closing delimiter
+        node = TextNode("Text with `code but no closing", TextType.TEXT)
+        with self.assertRaises(ValueError):
+            split_nodes_delimiter([node], "`", TextType.CODE)
+
+    def test_multiple_nodes(self):
+        # Test with multiple input nodes
+        nodes = [
+            TextNode("Text with `code`", TextType.TEXT),
+            TextNode("Plain text", TextType.TEXT),
+            TextNode("More `code blocks`", TextType.TEXT)
+        ]
+        new_nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+        self.assertEqual(len(new_nodes), 7)
+        # Check node 0: "Text with "
+        self.assertEqual(new_nodes[0].text, "Text with ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        # Check node 1: "code"
+        self.assertEqual(new_nodes[1].text, "code")
+        self.assertEqual(new_nodes[1].text_type, TextType.CODE)
+        # Check node 2: ""
+        self.assertEqual(new_nodes[2].text, "")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+        # Check node 3: "Plain text"
+        self.assertEqual(new_nodes[3].text, "Plain text")
+        self.assertEqual(new_nodes[3].text_type, TextType.TEXT)
+        # Check node 4: "More "
+        self.assertEqual(new_nodes[4].text, "More ")
+        self.assertEqual(new_nodes[4].text_type, TextType.TEXT)
+        # Check node 5: "code blocks"
+        self.assertEqual(new_nodes[5].text, "code blocks")
+        self.assertEqual(new_nodes[5].text_type, TextType.CODE)
+        # Check node 6: ""
+        self.assertEqual(new_nodes[6].text, "")
+        self.assertEqual(new_nodes[6].text_type, TextType.TEXT)
+        
 if __name__ == "__main__":
     unittest.main()
